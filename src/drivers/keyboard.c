e38b3ef9c09172f7e19235ccae32bb2827404eeb
Jacob Misirian <misirianrule@gmail.com>
2017-01-17 14:36:48 -0600

    Fixed shift with keyboard

--- src/drivers/keyboard.c
+++ src/drivers/keyboard.c
@@ -44,6 +44,51 @@ static const uint8_t kbdus [128] = {
     0,	/* All other keys are undefined */
 };
 
+static const uint8_t kbdus_shift [128] = {
+  0,                        //               0
+  27,                       // ESC ^[        1
+  '!', '@', '#', '$', '%',  // Numbers       2 -  6
+  '^', '&', '*', '(', ')',  // Numbers       7 - 11
+  '_', '+', '\b', '\t',     //              12 - 15
+  'Q', 'W', 'E', 'R', 'T',  // Chars        16 - 20
+  'Y', 'U', 'I', 'O', 'P',  // Chars        21 - 25
+  '{', '}', '\n',           //              26 - 28
+  0,                        // Control      29
+  'A', 'S', 'D', 'F', 'G',  // Chars        30 - 34
+  'H', 'J', 'K', 'L',       // Chars        35 - 38
+  ':', '\"', '`',           //              39 - 41
+  0,                        // Lshift       42
+  '|',                     //              43
+  'Z', 'X', 'C', 'V', 'B',  // Chars        44 - 48
+  'N', 'M',                 // Chars        49 - 50
+  '<', '>', '?',            //              51 - 53
+  0,                        // Rshift       54
+  '*',                      //              55
+  0,                        // Alt          56
+  ' ',                      // Space        57
+  0,                        // Caps lock    58
+  0, 0, 0, 0, 0,            // F1 - F5      59 - 63
+  0, 0, 0, 0, 0,            // F6 - F10     64 - 68
+  0,                        // Num lock     69
+  0,                        // Scroll lock  70
+  0,                        // Home key     71
+  0,                        // Arrow up     72
+  0,                        // Page up      73
+  '-',                      // Num minus    74
+  0,                        // Arrow left   75
+  0,                        //              76
+  0,                        // Arrow right  77
+  '+',                      // Num plus     78
+  0,                        // End          79
+  0,                        // Arrow down   80
+  0,                        // Page down    81
+  0,                        // Insert       82
+  0,                        // Delete       83
+  0, 0, 0,                  //              84 - 87
+  0, 0,                     // F11 - F12    88 - 89
+  0,                        //              90
+};
+
 static uint8_t buffer;
 static size_t count = 0;
 static size_t shiftPressed = 0;
@@ -99,13 +144,10 @@ keyEvent_t * keyboard_scancodeToKeyEvent (uint8_t key) {
 	res->keyChar = kbdus [key];
 	res->keyCharShift = res->keyChar;
 	res->pressType = pressType;
+	res->scanCode = key;
 	
 	if (shiftPressed == 1) {
-		char temp [2];
-		temp [0] = res->keyChar;
-		temp [1] = 0;
-		strup (temp);
-		res->keyCharShift = temp [0];
+		res->keyCharShift = kbdus_shift [key];
 	}
 	
 	return res;
